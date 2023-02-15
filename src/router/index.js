b5b9bb89b2ceee0623ab260195ebce962d93f03c
Bruno Ervilha <bruno.ervilha@landapp.com.br>
2023-02-15 19:21:57 -0300

    temporary repair

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,7 @@ router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
   const isAuthenticated = firebase.auth().currentUser;
   if (requiresAuth && !isAuthenticated) {
+    alert('Please login or register');
     next('/');
   } else {
     next();
